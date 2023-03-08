e3947e6e56640d107c24c951ca304ddfa2168fe0
AndryX-08 <90835105+AndryX-08@users.noreply.github.com>
2023-03-08 19:36:42 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function lightOn(){
+  window.open("https://maker.ifttt.com/trigger/light_on/json/with/key/cPrSJ64PQr0phdZryZ7BED");
+  window.history.back();
+}
+
+function lightOff(){
+  window.open("https://maker.ifttt.com/trigger/light_off/json/with/key/cPrSJ64PQr0phdZryZ7BED");
+  window.history.back();
+}
